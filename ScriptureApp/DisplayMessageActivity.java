23906773a36c09614170bb747ea4bfefaa76c487
kpwahn <kpwahnschaffe@gmail.com>
2015-02-07 15:49:59 -0700

    Scripture App

---
+++ ScriptureApp/DisplayMessageActivity.java
@@ -0,0 +1,78 @@
+package com.example.kendall.scriptureapp;
+
+import android.content.Intent;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class DisplayMessageActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Up button
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //Get The message from the Intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        //Create the text view
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText("Your favorite scripture is:\n" + message);
+
+        // Set the text view as the activity layout
+        setContentView(textView);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
+
+/*
+<activity   android:parentActivityName="com.example.kendall.scriptureapp" >
+    <meta-data
+    android:name="android.support.PARENT_ACTIVITY"
+    android:value="com.example.myfirstapp.MainActivity" />
+    </activity>
+ */
+/*
+switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                Intent upIntent = NavUtils.getParentActivityIntent(this);
+                if (NavUtils.shouldUpRecreateTask(this, upIntent)) {
+                    // This activity is NOT part of this app's task, so create a new task
+                    // when navigating up, with a synthesized back stack.
+                    TaskStackBuilder.create(this)
+                            // Add all of this activity's parents to the back stack
+                            .addNextIntentWithParentStack(upIntent)
+                                    // Navigate up to the closest parent
+                            .startActivities();
+                } else {
+                    // This activity is part of this app's task, so simply
+                    // navigate up to the logical parent activity.
+                    NavUtils.navigateUpTo(this, upIntent);
+                }
+                return true;
+        }
+ */
